7c1786c9887e2b21a93b1d017c39e472d085499c
jian <1184880285@qq.com>
2022-10-20 00:48:27 +0800

    HW2 completed version1

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,78 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF setforfull, setforper;
+    private boolean[][] array;
+    private int numberofopen, top, bottom;
+    public Percolation(int N){
+        if (N <= 0){
+            throw new java.lang.IllegalArgumentException("N can't <= 0");
+        }
+        numberofopen = 0;
+        array = new boolean[N][N];
+        setforfull = new WeightedQuickUnionUF(N * N + 1);
+        setforper = new WeightedQuickUnionUF(N * N + 2);
+        top = N * N;
+        bottom = N * N + 1;
+        for (boolean[] i : array) {
+            for (boolean j : i) {
+                j = false;
+            }
+        }
+    }
+    public void open(int row, int col){
+        if (row < 0 || col < 0 || row >= array.length || col >= array.length){
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        if (isOpen(row, col)) return;
+        array[row][col] = true;
+        if (row == 0){
+            setforper.union(top, xyToNumber(row, col));
+            setforfull.union(top, xyToNumber(row, col));
+        }
+        if (row == array.length - 1){
+            setforper.union(bottom, xyToNumber(row, col));
+        }
+        if (row > 0 && isOpen(row - 1, col)) {
+            setforper.union(xyToNumber(row, col), xyToNumber(row - 1, col));
+            setforfull.union(xyToNumber(row, col), xyToNumber(row - 1, col));
+        }
+        if (row < array.length - 1 && isOpen(row + 1, col)) {
+            setforper.union(xyToNumber(row, col), xyToNumber(row + 1, col));
+            setforfull.union(xyToNumber(row, col), xyToNumber(row + 1, col));
+        }
+        if (col > 0 && isOpen(row, col - 1)) {
+            setforper.union(xyToNumber(row, col), xyToNumber(row, col - 1));
+            setforfull.union(xyToNumber(row, col), xyToNumber(row, col - 1));
+        }
+        if (col < array.length - 1 && isOpen(row, col + 1)) {
+            setforper.union(xyToNumber(row, col), xyToNumber(row, col + 1));
+            setforfull.union(xyToNumber(row, col), xyToNumber(row, col + 1));
+        }
+    }
+
+    public boolean isOpen(int row, int col){
+        if (row < 0 || col < 0 || row >= array.length || col >= array.length){
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return array[row][col];
+    }
+    public boolean isFull(int row, int col){
+        if (row < 0 || col < 0 || row >= array.length || col >= array.length){
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return setforfull.connected(xyToNumber(row, col), top);
+    }
+
+    private int xyToNumber(int row, int col){
+       return row * array.length + col;
+    }
+    public int numberOfOpenSites(){
+        return numberofopen;
+    }
+
+    public boolean percolates(){
+        return setforper.connected(top, bottom);
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -2,4 +2,35 @@ package hw2;
 
 public class PercolationStats {
 
+    private int[] x;
+    Percolation set;
+    public  PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0){
+            throw new java.lang.IllegalArgumentException();
+        }
+        x = new int[T];
+        for (int i = 0; i < T; i++) {
+            set = pf.make(N);
+            while (set.percolates()) {
+              int randomx = edu.princeton.cs.introcs.StdRandom.uniform(0, N);
+              int randomy = edu.princeton.cs.introcs.StdRandom.uniform(0, N);
+              set.open(randomx, randomy);
+           }
+            x[i] = set.numberOfOpenSites();
+       }
+    }
+    public double mean() {
+        return edu.princeton.cs.introcs.StdStats.mean(x);
+    }
+    public double stddev() {
+        return edu.princeton.cs.introcs.StdStats.stddev(x);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(x.length);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(x.length);
+    }
 }
